2e60bf422934f4d57da262c2d4ef7795f2160a35
Timothy Russo <russotimothy@live.it>
2025-06-19 07:26:05 +0200

    feat: basic hotel data model

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stacks } from "@/src/ui/constants/navigation/Routes";
 import { ScreenOptions } from "@/src/ui/constants/navigation/ScreenOptions";
+import { queryClient } from "@/src/ui/query/QueryConfig";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
 	return (
-		<Stack screenOptions={ScreenOptions}>
-			<Stack.Screen name={Stacks.Main} />
-		</Stack>
+		<QueryClientProvider client={queryClient}>
+			<Stack screenOptions={ScreenOptions}>
+				<Stack.Screen name={Stacks.Main} />
+			</Stack>
+		</QueryClientProvider>
 	);
 }
